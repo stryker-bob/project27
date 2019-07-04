88e549639916c7cad9e20c6edde210f64368f625
Rubas <stryker-bob@mail.ru>
2019-07-04 20:49:57 +0600

    Initial Commit

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('tetle', 'Create Task')
+
+@section('content')
+
+<div class="row">
+    <col-wm-12>
+    <h1>Create Task</h1>
+    </col-wm-12>
+    </div>
+
+@endsection
